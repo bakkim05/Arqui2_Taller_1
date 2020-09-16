d0bc28331343cfa5cc9d30e6a6d0f19721f9b7e1
Jung <bakkim05@gmail.com>
2020-09-16 11:43:41 -0700

    mutex finalizado

--- parte2.c
+++ parte2.c
@@ -3,23 +3,33 @@
 #include <unistd.h>
 #include <pthread.h>
 
-int i = 0;
-int end = 1000;
+pthread_t thread_id[1];
+pthread_mutex_t lock;
+int process;
 
 void *Thread(void *vargp)
 {
-    while (i < end){
-        i++;
+    pthread_mutex_lock(&lock);
+    process += 1;
+    
+    printf("Proceso %d iniciado\n", process);
+    for (int i=0; i < 100 ; i++){
+        //does nothing
     }
+    printf("Proceso %d terminado\n", process);
+
+    pthread_mutex_unlock(&lock);
+
     return NULL;
 }
 
 
 int main ()
 {
-    pthread_t thread_id[1];
+    pthread_mutex_init(&lock,NULL);
     pthread_create(&thread_id[0], NULL, Thread, NULL);
     pthread_create(&thread_id[1], NULL, Thread, NULL);
     pthread_join(thread_id[0], NULL);
     pthread_join(thread_id[1], NULL);
+    pthread_mutex_destroy(&lock); 
 }
